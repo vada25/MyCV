beb80a6052ad7c6072702d0641ffd24ca43d1b47
vada25 <124167156+vada25@users.noreply.github.com>
2024-05-13 16:05:16 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+document.getElementById('qrCode').addEventListener('mouseenter', function() {
+    this.style.left = '0';
+});
+
+document.getElementById('qrCode').addEventListener('mouseleave', function() {
+    this.style.left = '-60px';
+});
